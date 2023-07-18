101a1f703334bd33cac5579e7f3d7ecc4e52cd5d
0x101 <0x101@0x101.local>
2023-07-18 21:21:57 +0200

    Make mongo client global

--- db/db.go
+++ db/db.go
@@ -5,23 +5,29 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func Connect() {
-	host := os.Getenv("HOST")
-	username := os.Getenv("USERNAME")
-	password := os.Getenv("PASSWORD")
-	database := os.Getenv("DATABASE")
-	connectionStr := fmt.Sprintf("mongodb+srv://%s:%s@%s/%s", username, password, host, database)
-	clientOptions := options.Client().ApplyURI(connectionStr)
-	client, err := mongo.Connect(context.Background(), clientOptions)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("[SUCCESS] Connected successfully to MongoDB %s\n", host)
-	defer client.Disconnect(context.Background())
+var MongoClient *mongo.Client
+var clientOnce sync.Once
 
+func Connect() {
+	clientOnce.Do(func() {
+		host := os.Getenv("HOST")
+		username := os.Getenv("USERNAME")
+		password := os.Getenv("PASSWORD")
+		database := os.Getenv("DATABASE")
+		connectionStr := fmt.Sprintf("mongodb+srv://%s:%s@%s/%s", username, password, host, database)
+		clientOptions := options.Client().ApplyURI(connectionStr)
+		client, err := mongo.Connect(context.Background(), clientOptions)
+		if err != nil {
+			log.Fatal(err)
+		}
+		MongoClient = client
+		fmt.Printf("[SUCCESS] Connected successfully to MongoDB %s\n", host)
+		defer client.Disconnect(context.Background())
+	})
 }
