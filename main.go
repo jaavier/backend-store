ad6e1c8365af5a4ddf7795feb242f034ff139bf1
Javier Guajardo <javier.guajardoj@gmail.com>
2023-07-19 08:25:06 +0200

    Add route for kitchen and pay order

--- main.go
+++ main.go
@@ -22,12 +22,17 @@ func main() {
 
 	// Routes for orders
 	e.GET("/orders/:orderId", handlers.ViewOrder, middlewares.JwtMiddleware)
+	e.GET("/orders/:orderId/pay", handlers.PayOrder, middlewares.JwtMiddleware)
 	e.GET("/orders", handlers.ViewAllOrders, middlewares.JwtMiddleware)
 	e.POST("/orders", handlers.CreateOrder, middlewares.JwtMiddleware)
 
 	// Routes for users
 	e.POST("/register", handlers.RegisterUser)
 	e.POST("/login", handlers.LoginUser)
+
+	// Routes for kitchen
+	e.GET("/kitchen/orders", handlers.GetOrdersKitchen)      // add middleware jwt
+	e.POST("/kitchen/orders/:orderId", handlers.MarkOrderAs) // add middleware jwt
 
 	e.Logger.Fatal(e.Start(":3000"))
 }
