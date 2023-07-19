8bf75767573e905c7f55a8f9eda5278fbb5abba2
Javier Guajardo <javier.guajardoj@gmail.com>
2023-07-19 05:05:48 +0200

    Add comments

--- main.go
+++ main.go
@@ -15,11 +15,17 @@ func main() {
 	var e = echo.New()
 
 	e.GET("/", handlers.PrivateHandler, middlewares.JwtMiddleware)
+
+	// Routes for products
 	e.GET("/products", handlers.LoadProducts)
 	e.POST("/products", handlers.InsertProduct, middlewares.JwtMiddleware)
+
+	// Routes for orders
 	e.GET("/orders/:orderId", handlers.ViewOrder, middlewares.JwtMiddleware)
 	e.GET("/orders", handlers.ViewAllOrders, middlewares.JwtMiddleware)
 	e.POST("/orders", handlers.CreateOrder, middlewares.JwtMiddleware)
+
+	// Routes for users
 	e.POST("/register", handlers.RegisterUser)
 	e.POST("/login", handlers.LoginUser)
 
