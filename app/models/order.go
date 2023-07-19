257988322981bf38df1808bf0b0c7dcbca3eb77b
Javier Guajardo <javier.guajardoj@gmail.com>
2023-07-19 03:15:31 +0200

    Add 'Total' to model Order

--- app/models/order.go
+++ app/models/order.go
@@ -6,4 +6,5 @@ type Order struct {
 	Products []string  `json:"productsIds" bson:"productsIds"`
 	Date     time.Time `json:"date" bson:"date"`
 	Id       string    `json:"orderId" bson:"orderId"`
+	Total    float64   `json:"total" bson:"total"`
 }
